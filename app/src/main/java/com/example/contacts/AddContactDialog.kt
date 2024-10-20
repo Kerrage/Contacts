39c752ff1611cb67d2ff8dab92b4df3a68207e9f
Jamie Kerr <jamie.kerr.47@gmail.com>
2024-10-20 16:14:44 +0100

    Create add contact dialog

--- app/src/main/java/com/example/contacts/AddContactDialog.kt
+++ app/src/main/java/com/example/contacts/AddContactDialog.kt
@@ -4,62 +4,80 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun AlertContent(
+    state: ContactState,
+    onEvent: (ContactEvent) -> Unit,
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        TextField(
+            value = state.firstName,
+            onValueChange = {
+                onEvent(ContactEvent.SetFirstName(it))
+            },
+            placeholder = {
+                Text(text = "First Name")
+            }
+        )
+        TextField(
+            value = state.lastName,
+            onValueChange = {
+                onEvent(ContactEvent.SetLastName(it))
+            },
+            placeholder = {
+                Text(text = "Last Name")
+            }
+        )
+        TextField(
+            value = state.phoneNumber,
+            onValueChange = {
+                onEvent(ContactEvent.SetPhoneNumber(it))
+            },
+            placeholder = {
+                Text(text = "Phone Number")
+            }
+        )
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.CenterEnd
+        ) {
+            Button(onClick = {
+                onEvent(ContactEvent.SaveContact)
+            }) {
+                Text(text = "Save contact")
+            }
+        }
+    }
+}
 
 @Suppress("DEPRECATION")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddContactDialog(
     state: ContactState,
-    oneEvent: (ContactEvent) -> Unit,
+    onEvent: (ContactEvent) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    AlertDialog(
-        onDismissRequest = {},
-        content = {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                TextField(
-                    value = state.firstName,
-                    onValueChange = {
-                        onEvent(ContactEvent.SetFirstName(it))
-                    },
-                    placeholder = {
-                        Text(text = "First Name")
-                    }
-                )
-                TextField(
-                    value = state.lastName,
-                    onValueChange = {
-                        onEvent(ContactEvent.SetLastName(it))
-                    },
-                    placeholder = {
-                        Text(text = "Last Name")
-                    }
-                )
-                TextField(
-                    value = state.phoneNumber,
-                    onValueChange = {
-                        onEvent(ContactEvent.SetPhoneNumber(it))
-                    },
-                    placeholder = {
-                        Text(text = "Phone Number")
-                    }
-                )
-            }
+    BasicAlertDialog(
+        modifier = modifier,
+        onDismissRequest = {
+            onEvent(ContactEvent.HideDialog)
         },
-        buttons = {
-            Box(modifier = Modifier.fillMaxWidth()) {
-                Button(onClick =)
-            }
+        content = @Composable {
+            AlertContent(state, onEvent)
         }
     )
 }
