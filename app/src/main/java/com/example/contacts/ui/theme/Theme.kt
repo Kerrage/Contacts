8bc2297545daddb651ec533e1c6bf29b5cdc724d
Jamie Kerr <jamie.kerr.47@gmail.com>
2024-10-22 18:51:35 +0100

    Follow coding recommendation

--- app/src/main/java/com/example/contacts/ui/theme/Theme.kt
+++ app/src/main/java/com/example/contacts/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.contacts.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -41,7 +40,7 @@ fun ContactsTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor && true -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
